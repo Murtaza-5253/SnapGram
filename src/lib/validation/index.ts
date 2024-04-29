4283c84366f579fa33fddd548686f3707d4431b1
murtaza <murtazamaimoon73@gmail.com>
2024-04-29 22:55:04 +0530

    Add Create Post Module Add File Upload functionality

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -12,3 +12,9 @@ export const SignInValidation = z.object({
   password: z.string().min(8, "Password must be atleast 8 characters"),
 });
 
+export const PostValidation = z.object({
+  caption:z.string().min(5).max(22),
+  file:z.custom<File[]>(),
+  location:z.string().min(2).max(100),
+  tags:z.string()
+})
